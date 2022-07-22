f66513ead3384356f907c7c6bb715dfb7ded32f2
hurainjavaid <hurainhoor889@gmail.com>
2022-07-22 12:06:52 +0530

    impl service

--- src/main/java/Controller/NewsController.java
+++ src/main/java/Controller/NewsController.java
@@ -1,9 +1,23 @@
 package Controller;
 
+import Entity.News;
+import com.rsl.news.RisingKashmir.Service.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/news")
 public class NewsController {
+    @Autowired
+    NewsService newsService;
+
+    @PostMapping("/save")
+    public ResponseEntity save(@RequestBody News save){
+     return ResponseEntity.status(HttpStatus.OK).body(newsService.saveNews(save));
+    }
 }
